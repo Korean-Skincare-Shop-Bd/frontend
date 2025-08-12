07fd3e3c2dd7e1da7f2585fc1e293c71f269bc0d
Jamal Uddin Mallick <103067313+jamal-uddin5823@users.noreply.github.com>
2025-08-12 20:48:04 +0600

    Fixed header logo

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -333,10 +333,10 @@ export function Header() {
               className="flex justify-center items-center bg-white rounded-full w-12 h-12">
               <Image
                 src="/logo.png"
-                width={25}
-                height={25}
+                width={50}
+                height={50}
                 alt="Korean Skincare Shop BD Logo"
-                className="pt-1 w-12 h-8"
+                className="pt-1 w-13 h-12 object-contain"
               />
             </motion.div>
             <span className="hidden lg:inline-block font-bold text-xl lg:text-2xl golden-text">
